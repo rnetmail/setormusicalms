db537b362c2c8c66887d746d16bc07d3c9e54737
Robson Souza <rnetmail@gmail.com>
2025-07-06 22:10:18 -0400

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,10 +1,5 @@
-# backend/backend/urls.py
-
-from django.contrib import admin
-from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Qualquer URL que comece com 'api/' será enviada para o ficheiro de URLs da nossa app 'api'
+    # CORRETO: Envia tudo que for /api/ para o ficheiro da app 'api'
     path('api/', include('api.urls')),
 ]
