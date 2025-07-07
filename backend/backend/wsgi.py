f95c93d3c12c0c6079c031b10cd16c921be6fbdf
Robson Souza <rnetmail@gmail.com>
2025-07-06 21:14:23 -0400

    Update wsgi.py

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,1 +1,9 @@
-Šjh®Ú,~º&v6§‚¢·
+# backend/backend/wsgi.py
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+
+application = get_wsgi_application()
