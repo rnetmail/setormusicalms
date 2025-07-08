abc5bd6fbe4b07b19462847e887fdf476f090623
Robson Souza <rnetmail@gmail.com>
2025-07-07 22:53:04 -0400

    Update api.ts

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import { RepertorioItem, AgendaItem, RecadoItem, User, GroupType } from '../types';
 
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = '/api';
 
 const getAuthToken = () => sessionStorage.getItem('authToken');
 
