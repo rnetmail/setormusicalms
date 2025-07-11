7ffabd04c742b2e9402023662342a2daa8818bad
Robson Souza <rnetmail@gmail.com>
2025-07-10 22:16:47 -0400

    Create __init__.py

---
+++ fastapi_backend/models/__init__.py
@@ -0,0 +1,6 @@
+from .user import User
+from .repertorio import RepertorioItem
+from .agenda import AgendaItem
+from .recado import RecadoItem
+
+__all__ = ["User", "RepertorioItem", "AgendaItem", "RecadoItem"]
