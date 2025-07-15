958bf8a55bc154febebdc36c80f0dd08c6ac6b4d
Robson Souza <rnetmail@gmail.com>
2025-07-15 11:23:06 -0400

    Deploy to VPS - VS Code #8

--- routers/auth.py
+++ routers/auth.py
@@ -1,9 +1,10 @@
 # setormusicalms\routers\auth.py
 from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+from sqlalchemy.orm import Session
+from database.database import get_db
+from crud import crud_user
+from security import security
 
 router = APIRouter(
     prefix="/api/auth",
@@ -11,17 +12,13 @@ router = APIRouter(
 )
 
 @router.post("/token")
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    # ATENÇÃO: Lógica de autenticação FAKE para desenvolvimento.
-    # A lógica de validação real seria assim:
-    # 1. Buscar o usuário no banco de dados pelo form_data.username
-    # 2. Se o usuário existir, verificar a senha com: pwd_context.verify(form_data.password, user.hashed_password)
-    
-    # Por enquanto, mantemos a lógica simples para validação do endpoint.
-    is_password_correct = pwd_context.verify("admin", pwd_context.hash("admin")) # Simula uma verificação
-    if form_data.username == "admin" and is_password_correct:
-        # Simula a criação de um token de acesso
-        access_token = f"fake-token-for-user-{form_data.username}"
-        return {"access_token": access_token, "token_type": "bearer"}
-    
-    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Usuário ou senha incorretos")
+async def login_for_access_token(db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()):
+    user = crud_user.get_user_by_username(db, username=form_data.username)
+    if not user or not security.verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Usuário ou senha incorretos",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token = security.create_access_token(data={"sub": user.username})
+    return {"access_token": access_token, "token_type": "bearer"}
