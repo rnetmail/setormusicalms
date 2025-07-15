3277aebc4ca9a9f9fcc752f1951c7aa5bd12089a
Robson Souza <rnetmail@gmail.com>
2025-07-15 15:27:12 -0400

    Update init_admin.py

--- backend/init_admin.py
+++ backend/init_admin.py
@@ -1,16 +1,21 @@
 # setormusicalms\backend\init_admin.py
 
-from database.database import SessionLocal, engine, Base
-from models.user import User
-from security.security import get_password_hash
-
 # Senha padrão para o usuário admin
 ADMIN_PASSWORD = "Setor@MS25"
 
+def init_db():
+    Base.metadata.create_all(bind=engine)
+    db = SessionLocal()
+    admin_user = db.query(User).filter(User.username == "admin").first()
+    if not admin_user:
+        hashed_password = get_password_hash(ADMIN_PASSWORD)
+        admin_user = User(username="admin", hashed_password=hashed_password, is_superuser=True, is_staff=True, active=True)
         db.add(admin_user)
         db.commit()
-        print(f"Username: {admin_user.username}")
-        print("Password: Setor@MS25")
-        print(f"is_staff: {admin_user.is_staff}")
-        print(f"is_superuser: {admin_user.is_superuser}")
+        print(f"Admin user created: Username: {admin_user.username}, Password: {ADMIN_PASSWORD}")
     else:
+        print(f"Admin user '{admin_user.username}' already exists.")
+    db.close()
+
+if __name__ == "__main__":
+    init_db()
