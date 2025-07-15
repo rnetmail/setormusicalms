3fdfb44727183c39a3638dfcc722fa19d2a76f12
Robson Souza <rnetmail@gmail.com>
2025-07-15 11:09:13 -0400

    Deploy to VPS - VS Code #6

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-# \setormusicalms\main.py
+# setormusicalms\main.py
 from fastapi import FastAPI
+from routers import auth
+from database.database import engine, Base
+import models.user # Importa o módulo para que o SQLAlchemy o reconheça
+
+models.user.Base = Base
+
+# Cria todas as tabelas no banco de dados (se não existirem)
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Setor Musical MS",
     description="Microserviço para gerenciar entidades do setor musical.",
-    version="1.0.0"
+    version="1.0.0",
 )
+
+# Inclui o roteador de autenticação na aplicação principal
+app.include_router(auth.router)
 
 @app.get("/")
 def read_root():
