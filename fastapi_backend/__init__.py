5620c98d7aeb2e861ba29ad3aa5ba3f155d91ed2
Robson Souza <rnetmail@gmail.com>
2025-07-17 10:09:56 -0400

    Create __init__.py

---
+++ fastapi_backend/__init__.py
@@ -0,0 +1,5 @@
+# fastapi_backend/__init__.py
+# Versão 75 18/07/2025 09:05
+
+# Este ficheiro pode ficar vazio. A sua simples existência
+# indica ao Python que a pasta 'fastapi_backend' é um pacote.
