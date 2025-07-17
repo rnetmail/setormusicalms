19133962165fa568c573636481337a29aa2a0ac9
Robson Souza <rnetmail@gmail.com>
2025-07-17 10:15:47 -0400

    Update __init__.py

--- fastapi_backend/models/__init__.py
+++ fastapi_backend/models/__init__.py
@@ -1,3 +1,8 @@
+# fastapi_backend/models/__init__.py
+# Versão 76 18/07/2025 09:10
+
+# Este ficheiro importa todos os modelos para que possam ser
+# facilmente acedidos a partir do pacote 'models'.
 from .user import User
 from .repertorio import RepertorioItem
 from .agenda import AgendaItem
