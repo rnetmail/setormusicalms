586a803a9a51732754473275e912d232c7cc30bd
Robson Souza <rnetmail@gmail.com>
2025-07-16 20:52:34 -0400

    Delete backend directory

--- backend/crud/crud_user.py
+++
@@ -1,6 +0,0 @@
-# setormusicalms\backend\crud\crud_user.py
-from sqlalchemy.orm import Session
-import models.user as models
-
-def get_user_by_username(db: Session, username: str):
-    return db.query(models.User).filter(models.User.username == username).first()
--- backend/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .user import User
-from .repertorio import RepertorioItem
-from .agenda import AgendaItem
-from .recado import RecadoItem
-
-__all__ = ["User", "RepertorioItem", "AgendaItem", "RecadoItem"]
-
