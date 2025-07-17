dcc30739e0a2b240715ee93968de4770f4b8d345
Robson Souza <rnetmail@gmail.com>
2025-07-17 17:03:16 -0400

    Update repertorio.py

--- fastapi_backend/models/repertorio.py
+++ fastapi_backend/models/repertorio.py
@@ -1,5 +1,5 @@
 # fastapi_backend/models/repertorio.py
-# Versão 18 16/07/2025 22:11
+# Versão 14 17/07/2025 17:18
 from sqlalchemy import Column, Integer, String, Boolean, JSON, Text
 from app.database import Base
 
@@ -15,12 +15,14 @@ class RepertorioItem(Base):
     arrangement = Column(String(200), nullable=True)
     year = Column(Integer, nullable=False)
     
-    # Usamos o tipo Text para permitir URLs longas.
+    # Usamos o tipo Text para permitir URLs longas e variadas.
     audio_url = Column(Text, nullable=True)
     video_url = Column(Text, nullable=True)
+    # NOVO CAMPO: Armazena a URL da miniatura do vídeo, extraída pelo backend.
+    video_thumbnail_url = Column(Text, nullable=True)
     sheet_music_url = Column(Text, nullable=False)
     
-    # O tipo JSON é ideal para armazenar listas de strings, como naipes ou grupos.
+    # O tipo JSON é ideal para armazenar listas flexíveis de strings.
     naipes = Column(JSON, default=list)  # Para o Coral
     grupos = Column(JSON, default=list)  # Para a Orquestra
     
