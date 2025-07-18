663cecd27c5079853a1e09f3daa8d3bfcfdc00df
Robson Souza <rnetmail@gmail.com>
2025-07-17 23:54:29 -0400

    Update agenda.py

--- fastapi_backend/crud/agenda.py
+++ fastapi_backend/crud/agenda.py
@@ -1,5 +1,5 @@
 # fastapi_backend/crud/agenda.py
-# Versão 10 17/07/2025 23:55
+# Versão 24 18/07/2025 00:20
 from sqlalchemy.orm import Session
 from typing import List, Optional
 
@@ -32,4 +32,29 @@ def get_agenda_items(
     
     return query.order_by(AgendaItem.date.desc()).offset(skip).limit(limit).all()
 
-def create_agenda_
+def create_agenda_item(db: Session, item: AgendaItemCreate) -> AgendaItem:
+    """Cria um novo item na agenda."""
+    db_item = AgendaItem(**item.model_dump())
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item
+
+def update_agenda_item(db: Session, item_id: int, item_update: AgendaItemUpdate) -> Optional[AgendaItem]:
+    """Atualiza um item da agenda existente."""
+    db_item = db.query(AgendaItem).filter(AgendaItem.id == item_id).first()
+    if db_item:
+        update_data = item_update.model_dump(exclude_unset=True)
+        for field, value in update_data.items():
+            setattr(db_item, field, value)
+        db.commit()
+        db.refresh(db_item)
+    return db_item
+
+def delete_agenda_item(db: Session, item_id: int) -> Optional[AgendaItem]:
+    """Remove um item da agenda do banco de dados."""
+    db_item = db.query(AgendaItem).filter(AgendaItem.id == item_id).first()
+    if db_item:
+        db.delete(db_item)
+        db.commit()
+    return db_item
