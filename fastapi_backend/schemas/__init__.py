d88c2d802129448ba2830bdf0847f46e51bbc675
Robson Souza <rnetmail@gmail.com>
2025-07-21 14:30:49 -0400

    Update __init__.py

--- fastapi_backend/schemas/__init__.py
+++ fastapi_backend/schemas/__init__.py
@@ -1,11 +1,12 @@
 # fastapi_backend/schemas/__init__.py
-# Versão 78 21/07/2025 11:03
+# Versão 79 21/07/2025 18:17
 
 from .user import User, UserCreate, UserUpdate, Token, TokenData
 from .repertorio import RepertorioItem, RepertorioItemCreate, RepertorioItemUpdate
 from .agenda import AgendaItem, AgendaItemCreate, AgendaItemUpdate
 from .recado import RecadoItem, RecadoItemCreate, RecadoItemUpdate
 from .historia import HistoriaItem, HistoriaItemCreate, HistoriaItemUpdate
+from .galeria import GaleriaItem, GaleriaItemCreate, GaleriaItemUpdate
 
 # A linha __all__ define o que é exportado quando alguém faz 'from schemas import *'
 __all__ = [
@@ -14,4 +15,5 @@ __all__ = [
     "AgendaItem", "AgendaItemCreate", "AgendaItemUpdate",
     "RecadoItem", "RecadoItemCreate", "RecadoItemUpdate",
     "HistoriaItem", "HistoriaItemCreate", "HistoriaItemUpdate",
+    "GaleriaItem", "GaleriaItemCreate", "GaleriaItemUpdate",
 ]
