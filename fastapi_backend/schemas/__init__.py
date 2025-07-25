4fccec30f08a3c9d7b4f5c717bbacd601c47066b
Robson Souza <rnetmail@gmail.com>
2025-07-25 17:12:08 -0400

    Update __init__.py

--- fastapi_backend/schemas/__init__.py
+++ fastapi_backend/schemas/__init__.py
@@ -1,12 +1,18 @@
 # fastapi_backend/schemas/__init__.py
-# Versão 01 25/07/2025 11:05
+# Versão 02 25/07/2025 14:28
 from .user import User, UserCreate, UserUpdate, Token, TokenData
+from .repertorio import RepertorioItem, RepertorioItemCreate, RepertorioItemUpdate
+from .agenda import AgendaItem, AgendaItemCreate, AgendaItemUpdate
+from .recado import RecadoItem, RecadoItemCreate, RecadoItemUpdate
+from .historia import HistoriaItem, HistoriaItemCreate, HistoriaItemUpdate
+from .galeria import GaleriaItem, GaleriaItemCreate, GaleriaItemUpdate
 
-# A lista __all__ define quais nomes são exportados publicamente pelo pacote.
+# A lista __all__ define o que é exportado quando se utiliza "from schemas import *"
 __all__ = [
-    "User",
-    "UserCreate",
-    "UserUpdate",
-    "Token",
-    "TokenData",
+    "User", "UserCreate", "UserUpdate", "Token", "TokenData",
+    "RepertorioItem", "RepertorioItemCreate", "RepertorioItemUpdate",
+    "AgendaItem", "AgendaItemCreate", "AgendaItemUpdate",
+    "RecadoItem", "RecadoItemCreate", "RecadoItemUpdate",
+    "HistoriaItem", "HistoriaItemCreate", "HistoriaItemUpdate",
+    "GaleriaItem", "GaleriaItemCreate", "GaleriaItemUpdate",
 ]
