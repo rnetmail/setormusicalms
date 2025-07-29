9fdf86561f69fbdecd6edb7240bee1bf80efe7c1
Robson Souza <rnetmail@gmail.com>
2025-07-28 22:08:15 -0400

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Esta opção otimiza o build para Docker, criando uma pasta 'standalone'
-  // com todas as dependências necessárias, resultando em imagens menores.
-  output: 'standalone',
-};
-
-export default nextConfig;
