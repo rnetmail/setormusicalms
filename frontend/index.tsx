321139d6218dab52ba1ac14afdb992fcb094d345
Robson Souza <rnetmail@gmail.com>
2025-08-07 13:49:50 -0400

    Update index.tsx

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,16 +1,21 @@
+// /frontend/index.tsx
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+
+// CORREÇÃO: O caminho para App.tsx deve incluir o diretório 'src'.
+import App from './src/App.tsx';
+
+// O resto do arquivo permanece o mesmo...
+// (Pode haver outras importações aqui, como o seu CSS)
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Elemento com id 'root' não encontrado no DOM.");
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
