a567b01547d45aa1529444965f5204c1a1004c1c
Robson Souza <rnetmail@gmail.com>
2025-08-07 21:27:34 -0400

    Delete fastapi_backend/app directory

--- fastapi_backend/app/routers/__init__.py
+++
@@ -1,5 +0,0 @@
-# fastapi_backend/__init__.py
-# Versão 75 18/07/2025 09:05
-
-# Este ficheiro pode ficar vazio. A sua simples existência
-# indica ao Python que a pasta 'fastapi_backend/app/routers' é um pacote.
